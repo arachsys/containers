cd1c2d94e6711b6b5fe3b6eea4c0f5f84ebac9d0
Chris Webb <chris@arachsys.com>
2013-07-02 15:32:14 +0100

    Import initial versions of contain and pseudo

---
+++ contain.c
@@ -0,0 +1,122 @@
+#define _GNU_SOURCE
+#include <errno.h>
+#include <error.h>
+#include <fcntl.h>
+#include <grp.h>
+#include <sched.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sysexits.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include "contain.h"
+
+void usage(char *progname) {
+  fprintf(stderr, "\
+Usage: %s [OPTIONS] DIR [CMD [ARG]...]\n\
+Options:\n\
+  -g MAP    set the container-to-host GID map\n\
+  -i PATH   run a helper child inside the new namespaces\n\
+  -n        share the host network unprivileged in the container\n\
+  -o PATH   run a helper child outside the new namespaces\n\
+  -u MAP    set the container-to-host UID map\n\
+GID and UID maps are specified as START:LOWER:COUNT[,START:LOWER:COUNT]...\n\
+", progname);
+  exit(EX_USAGE);
+}
+
+int main(int argc, char **argv) {
+  char *gidmap = NULL, *inside = NULL, *outside = NULL, *uidmap = NULL;
+  int hostnet = 0, master, option;
+  pid_t child, parent;
+
+  while ((option = getopt(argc, argv, "+:g:i:no:u:")) > 0)
+    switch (option) {
+      case 'g':
+        gidmap = optarg;
+        break;
+      case 'i':
+        inside = optarg;
+        break;
+      case 'n':
+        hostnet++;
+        break;
+      case 'o':
+        outside = optarg;
+        break;
+      case 'u':
+        uidmap = optarg;
+        break;
+      default:
+        usage(argv[0]);
+    }
+
+  if (argc <= optind)
+    usage(argv[0]);
+
+  parent = getpid();
+  switch (child = fork()) {
+    case -1:
+      error(1, errno, "fork");
+    case 0:
+      raise(SIGSTOP);
+      writemap(parent, GID, gidmap);
+      writemap(parent, UID, uidmap);
+
+      if (outside) {
+        if (setgid(getgid()) < 0 || setuid(getuid()) < 0)
+          error(1, 0, "Failed to drop privileges");
+        putenv("container=contain-outside-helper");
+        execlp(outside, outside, NULL);
+        error(1, errno, "exec %s", outside);
+      }
+
+      exit(EXIT_SUCCESS);
+  }
+
+  if (setgid(getgid()) < 0 || setuid(getuid()) < 0)
+    error(1, 0, "Failed to drop privileges");
+
+  if (unshare(CLONE_NEWIPC | CLONE_NEWNS | CLONE_NEWUSER | CLONE_NEWUTS) < 0)
+    error(1, 0, "Failed to unshare namespaces");
+
+  if (!hostnet && unshare(CLONE_NEWNET) < 0)
+      error(1, 0, "Failed to unshare network namespace");
+
+  waitforstop(child);
+  kill(child, SIGCONT);
+  waitforexit(child);
+
+  setgid(0);
+  setgroups(0, NULL);
+  setuid(0);
+
+  master = getconsole();
+  buildroot(argv[optind], tmpdir(), ptsname(master), inside);
+
+  unshare(CLONE_NEWPID);
+  switch (child = fork()) {
+    case -1:
+      error(1, errno, "fork");
+    case 0:
+      close(master);
+      setconsole("/dev/console");
+
+      mountproc();
+      mountsys();
+
+      clearenv();
+      putenv("container=contain");
+
+      if (argv[optind + 1])
+        execv(argv[optind + 1], argv + optind + 1);
+      else
+        execl("/bin/sh", "/bin/sh", NULL);
+      error(1, errno, "exec");
+  }
+
+  return supervise(child, master);
+}
---
+++ contain.h
@@ -0,0 +1,26 @@
+#ifndef CONTAIN_H
+#define CONTAIN_H
+
+#define GID 0
+#define UID 1
+#define INVALID ((unsigned) -1)
+
+#define getid(type) ((unsigned) ((type) == GID ? getgid() : getuid()))
+#define idfile(type) ((type) == GID ? "gid_map" : "uid_map")
+#define idname(type) ((type) == GID ? "GID" : "UID")
+#define subpath(type) ((type) == GID ? "/etc/subgid" : "/etc/subuid")
+
+extern char *append(char **destination, const char *format, ...);
+extern void buildroot(char *src, char *dst, char *console, char *helper);
+extern int getconsole(void);
+extern void mountproc(void);
+extern void mountsys(void);
+extern void setconsole(char *name);
+extern char *string(const char *format, ...);
+extern int supervise(pid_t child, int console);
+extern char *tmpdir(void);
+extern void waitforstop(pid_t child);
+extern void waitforexit(pid_t child);
+extern void writemap(pid_t pid, int type, char *map);
+
+#endif
